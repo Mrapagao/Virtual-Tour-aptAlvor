431df6e10041ca20c017f69e622bc93d2a98162d
MRapagao <76593197+Mrapagao@users.noreply.github.com>
2023-12-31 19:57:43 +0000

    second pic upload

--- data.js
+++ data.js
@@ -28,7 +28,26 @@ var APP_DATA = {
         "pitch": 1.3965984102526363e-7,
         "fov": 1.3365071038314758
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.25188390520793646,
+          "pitch": 0.014040729697773813,
+          "rotation": 0,
+          "target": "4-livingroom"
+        },
+        {
+          "yaw": -2.094730582817972,
+          "pitch": 0.018705652942372808,
+          "rotation": 0,
+          "target": "1-storage"
+        },
+        {
+          "yaw": 1.0949253615637122,
+          "pitch": -0.0056386799036030055,
+          "rotation": 0,
+          "target": "2-hall-2"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -59,7 +78,14 @@ var APP_DATA = {
         "pitch": -0.015139724155044831,
         "fov": 1.3365071038314758
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -2.9829822143600673,
+          "pitch": 0.02997443968736846,
+          "rotation": 0,
+          "target": "0-hall-1"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -86,11 +112,30 @@ var APP_DATA = {
       ],
       "faceSize": 1680,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 1.1076127945348766,
+        "pitch": -0.011278716597104221,
+        "fov": 1.3365071038314758
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 0.49725196187460874,
+          "pitch": -0.001963888460499774,
+          "rotation": 0,
+          "target": "7-bedroom-1"
+        },
+        {
+          "yaw": 1.9541431135918588,
+          "pitch": 0.021304242015864006,
+          "rotation": 0,
+          "target": "3-bathroom"
+        },
+        {
+          "yaw": -1.6812597643041158,
+          "pitch": -0.007360585386742002,
+          "rotation": 0,
+          "target": "0-hall-1"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -152,7 +197,20 @@ var APP_DATA = {
         "pitch": -0.009398784133836102,
         "fov": 1.3365071038314758
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -1.113015508257238,
+          "pitch": -0.00834986216659317,
+          "rotation": 0,
+          "target": "5-kitchen"
+        },
+        {
+          "yaw": 1.5837980450954117,
+          "pitch": -0.03649489537360573,
+          "rotation": 0,
+          "target": "0-hall-1"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -183,7 +241,20 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -1.4356973679989942,
+          "pitch": -0.02689000451746182,
+          "rotation": 0,
+          "target": "6-kitchen-balcony"
+        },
+        {
+          "yaw": 2.2122847153630634,
+          "pitch": -0.00846001041325728,
+          "rotation": 0,
+          "target": "4-livingroom"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -214,7 +285,14 @@ var APP_DATA = {
         "pitch": 0,
         "fov": 1.3365071038314758
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -1.640878103965294,
+          "pitch": -0.004094386926752236,
+          "rotation": 0,
+          "target": "5-kitchen"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -245,7 +323,20 @@ var APP_DATA = {
         "pitch": 0,
         "fov": 1.3365071038314758
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 2.606221599452626,
+          "pitch": 0.03189425227103371,
+          "rotation": 0,
+          "target": "2-hall-2"
+        },
+        {
+          "yaw": 0.14237711315569257,
+          "pitch": 0.014501720997888157,
+          "rotation": 0,
+          "target": "8-bedroom-2"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -276,7 +367,20 @@ var APP_DATA = {
         "pitch": -0.011278540960603323,
         "fov": 1.3365071038314758
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 1.8781840991974734,
+          "pitch": 0.06370101615221557,
+          "rotation": 0,
+          "target": "7-bedroom-1"
+        },
+        {
+          "yaw": -2.00098860063682,
+          "pitch": -0.002335626775948896,
+          "rotation": 0,
+          "target": "9-bedroom-balcony"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -307,15 +411,22 @@ var APP_DATA = {
         "pitch": 0,
         "fov": 1.3365071038314758
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 3.054798753270358,
+          "pitch": 0.03373583413736725,
+          "rotation": 0,
+          "target": "8-bedroom-2"
+        }
+      ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Visita Virtual",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
